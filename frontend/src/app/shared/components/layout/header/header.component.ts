043519cc5ecb6fb4fa811f396501e436e15c9ab5
igorsoler.c <igorsoler.c@gmail.com>
2025-07-25 01:03:52 -0300

    feat: Criado Header da Aplicação

--- frontend/src/app/shared/components/layout/header/header.component.ts
+++ frontend/src/app/shared/components/layout/header/header.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../../core/services/theme.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Sprout, Plus } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
-    RouterModule 
+    RouterModule,
+    LucideAngularModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -15,13 +17,7 @@ import { RouterModule } from '@angular/router';
 export class HeaderComponent {
   themeService = inject(ThemeService);
   
-  isMenuOpen = false;
+  readonly Sprout = Sprout;
+  readonly Plus = Plus;
 
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  closeMenu(): void {
-    this.isMenuOpen = false;
-  }
 }
